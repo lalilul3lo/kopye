967c9b3e31f9a1f7b4feb600553488ee2c474790
Samuel Joli <samuel.joli.ftn@gmail.com>
2025-02-21 22:19:45 -0500

    style: lint

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::{crate_authors, crate_description, crate_name, crate_version, Command};
 
 fn main() {
-    let matches = Command::new(crate_name!())
+    let _matches = Command::new(crate_name!())
         .about(crate_description!())
         .author(crate_authors!())
         .version(crate_version!())
