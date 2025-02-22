891d52a9fcf0dc59017cf3e272bf3054f14c41b5
Samuel Joli <samuel.joli.ftn@gmail.com>
2025-02-22 01:35:19 -0500

    feat: implement basic copy and list subcommands

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
-pub fn hello() -> &'static str {
-    "hello"
+pub mod actions {
+    pub fn copy_template(_repo: &str, _template: &str, _dest: &str) {
+        //
+    }
+
+    pub fn list_templates(_repo: &str) {
+        //
+        println!("hello world");
+    }
 }
--- tests/integration.rs
+++ tests/integration.rs
@@ -1,6 +1,24 @@
-use kopye::hello;
+// Integration testing can be done either by calling library functions directly or by invoking your CLI as a subprocess.
+#[test]
+fn copy_template() {
+    let repo = "gh:lalilul3lo/dev";
+    let template = "rust";
+    let destination = "baouncer";
+    let mut cmd = assert_cmd::Command::cargo_bin("kopye").unwrap();
+
+    cmd.arg("copy").arg(repo).arg(template).arg(destination);
+
+    cmd.assert().success();
+}
 
 #[test]
-fn executes() {
-    assert_eq!(hello(), "hello")
+fn list_templates() {
+    let repo = "gh:lalilul3lo/dev";
+    let mut cmd = assert_cmd::Command::cargo_bin("kopye").unwrap();
+
+    cmd.arg("list").arg(repo);
+
+    cmd.assert()
+        .success()
+        .stdout(predicates::str::contains("hello world"));
 }
